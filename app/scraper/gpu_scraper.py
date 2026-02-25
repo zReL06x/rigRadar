3937d9d3f076d9adfbac9d4f1263bb8f2fdb2938
BL-User <179770272+zReL06x@users.noreply.github.com>
2026-02-25 04:53:47 +0000

    inital bs4 code

--- app/scraper/gpu_scraper.py
+++ app/scraper/gpu_scraper.py
@@ -1,32 +1,100 @@
-import requests
-import time
 from bs4 import BeautifulSoup
-
-url = "https://www.newegg.com/?srsltid=AfmBOopouwiE2fOlqybeD08dCePlu1RZyVd439yvxhDGqfUBLpNgQ9rn"
-
-# to avoid sites that blocks BOTS
-headers= {
-    "User-Agent": "Mozilla/5.0"
-}
-
-response = requests.get(url, headers=headers)
-
-if response.status_code != 200: print("failed")
-
-requests.get(url, timeout=10)
-time.sleep(2)
-
-soup = BeautifulSoup(response.text,   "lxml")
+import requests
 
 
-soup.find("h1")
-title = soup.find("div", "goods-title")
-price = soup.find("div", "goods-price-current")
+def main(URL):
+    # opening our output file in append mode
+    File = open("out.csv", "a")
 
-print(title, price)
+    # specifying user agent, You can use other user agents
+    # available on the internet
+    HEADERS = {
+        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+        'Accept-Language': 'en-US, en;q=0.5'
+    }
 
+    # Making the HTTP Request
+    webpage = requests.get(URL, headers=HEADERS)
 
+    # Creating the Soup Object containing all data
+    soup = BeautifulSoup(webpage.content, "lxml")
 
+    # retrieving product title
+    try:
+        # Outer Tag Object
+        title = soup.find("span", attrs={
+            'id': 'productTitle'
+        })
 
+        # Inner NavigableString Object
+        title_value = title.string
 
+        # Title as a string value
+        title_string = title_value.strip().replace(',', '')
 
+    except AttributeError:
+        title_string = "NA"
+    print("product Title = ", title_string)
+
+    # saving the title in the file
+    File.write(f"{title_string},")
+
+    # retrieving price
+    try:
+        price = soup.find("span", attrs={
+            'id': 'priceblock_ourprice'
+        }).string.strip().replace(',', '')
+        # we are omitting unnecessary spaces
+        # and commas form our string
+    except AttributeError:
+        price = "NA"
+    print("Products price = ", price)
+
+    # saving
+    File.write(f"{price},")
+
+    # retrieving product rating
+    try:
+        rating = soup.find("i", attrs={
+                           'class': 'a-icon a-icon-star a-star-4-5'
+        }).string.strip().replace(',', '')
+
+    except AttributeError:
+
+        try:
+            rating = soup.find("span", attrs={
+                'class': 'a-icon-alt'
+            }).string.strip().replace(',', '')
+        except:
+            rating = "NA"
+    print("Overall rating = ", rating)
+
+    File.write(f"{rating},")
+
+    try:
+        review_count = soup.find("span", attrs={
+            'id': 'acrCustomerReviewText'
+        }).string.strip().replace(',', '')
+
+    except AttributeError:
+        review_count = "NA"
+    print("Total reviews = ", review_count)
+    File.write(f"{review_count},")
+
+    # print availablility status
+    try:
+        available = soup.find("div", attrs={'id': 'availability'})
+        available = available.find("span").string.strip().replace(',', '')
+
+    except AttributeError:
+        available = "NA"
+    print("Availability = ", available)
+
+    # saving the availability and closing the line
+    File.write(f"{available},\n")
+
+    # closing the file
+    File.close()
+    
+    if __name__ == '__main__':
+        main("https://www.amazon.com/s?i=specialty-aps&bbn=16225007011&rh=n%3A16225007011%2Cn%3A193870011&ref=nav_em__nav_desktop_sa_intl_computer_components_0_2_7_3")
